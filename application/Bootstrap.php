a6ef81e9afa644c5dd1e843307b24044193811bd
wowpatrick <>
2014-09-23 14:24:32 +0200

    Added Zend Framework 1.12.9 Minimal as library and enabled the autoloader

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -8,4 +8,15 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $config = new Zend_Config_Ini('/path/to/config.ini',
             'staging');
     }
+    
+    /**
+     * Activate and enable the Zend autoloader for the application
+     * 
+     * @return Zend_Loader_Autoloader
+     */
+    public function _initAutoloader() {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        
+        return $autoloader;
+    }
 }
